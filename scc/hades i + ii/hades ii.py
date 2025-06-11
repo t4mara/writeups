9b8e27153be890e6575a468cebaa650da191d7d8
t4mara <tamarailic@elfak.rs>
2025-06-11 18:23:27 +0200

    scc stuff added

--- scc/hades i + ii/hades ii.py
+++ scc/hades i + ii/hades ii.py
@@ -24,8 +24,6 @@ i = 0
 j = 0
 for _ in range(0,50):
 	print("ROUND : ", i)
-	#user_input = input('Write...')
-	#input_bytes = user_input.encode('utf-8')
 	
 	sock.sendall((str(3) + "\n").encode('utf-8'))
 	sock.sendall(("3030303030303030303030303030303030303030303030303030303030303030" + "\n").encode('utf-8'))
@@ -33,11 +31,8 @@ for _ in range(0,50):
 	data = sock.recv(1024)
 	dec = data.decode()
 	text = dec.split()
+
 	
-	#for index,value in enumerate(text):
-		#print(index, " : ", value)
-	
-	#Gledamo levi samo
 	whole = text[10 + j]
 	first_half = whole[:32]
 	second_half = whole[32:]
@@ -48,20 +43,13 @@ for _ in range(0,50):
 	dec_first = int(first_half, 16)
 	dec_second = int(second_half, 16)
 
-	#print(dec_first)
-	#print(dec_second)
-
 	first_arr = aes.utils.int2arr8bit(dec_first, 16)
 	second_arr = aes.utils.int2arr8bit(dec_second, 16)
 
 	rez1 = cipher.dec_once(first_arr)
 	rez2 = cipher.enc_once(second_arr)
-
-	#print(rez1)
-	#print(rez2)
 	
 	if(rez1 == rez2):
-		# Ovo znaci da je levi pseud
 		print("Levi je pseud")
 		sock.sendall((str(2) + "\n").encode('utf-8'))
 	else:
